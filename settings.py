85e8d88bd0f05c17d2c1d418a62af3866635e714
Will Barton <william.barton@cfpb.gov>
2016-01-20 17:10:40 -0500

    Add regparser settings

--- settings.py
+++ settings.py
@@ -1,10 +1,8 @@
-__author__ = 'vinokurovy'
-
 import os
 
-XML_ROOT = '../regulations-schema/src'
-JSON_ROOT = '../regulations-xml-json/'
-XSD_FILE = os.path.join(XML_ROOT, 'eregs.xsd')
+XML_ROOT = '../regulations-stub/xml'
+JSON_ROOT = '../regulations-stub/stub'
+XSD_FILE = '../regulations-schema/src/eregs.xsd'
 
 # the inflect module has a few problems... manual override for that
 
@@ -12,3 +10,11 @@ SPECIAL_SINGULAR_NOUNS = [
     'bonus',
     'escrow account analysis'
 ]
+
+## eCFR Parser Settings
+from regparser.default_settings import *
+
+# OUTPUT_DIR=os.environ.get('OUTPUT_DIR', "../regulations-stub/stub/")
+OUTPUT_DIR=os.environ.get('OUTPUT_DIR', "../regulations-stub/xml/")
+# OUTPUT_DIR="../regulations-stub/stub/"
+LOCAL_XML_PATHS = ['../fr-notices/',]
