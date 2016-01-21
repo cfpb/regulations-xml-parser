bef4477c551a50ec53ded8aa75c7511f259e9586
Will Barton <william.barton@cfpb.gov>
2016-01-21 16:28:45 -0500

    Cleanup regal and import XML parsing code from reg_xml_parser

--- regml.py
+++ regml.py
@@ -37,13 +37,9 @@ from regparser.federalregister import fetch_notice_json
 from regparser.builder import Builder, LayerCacheAggregator, tree_and_builder
 from regparser.notice.build import check_local_version_list
 
-
 if (sys.version_info < (3, 0)):
     reload(sys)  # noqa
     sys.setdefaultencoding('UTF8')
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 # Utility Functions ####################################################
 
@@ -94,13 +90,80 @@ def write_layer(layer_object, reg_number, notice, layer_type,
     if diff_notice is not None:
         layer_path = os.path.join(layer_path, diff_notice)
     if not os.path.exists(layer_path):
-        os.mkdir(layer_path)
+        os.makedirs(layer_path)
     layer_file = os.path.join(layer_path, notice)
-    logger.info("writing", layer_file)
+    print("writing", layer_file)
     json.dump(layer_object, open(layer_file, 'w'), indent=4,
               separators=(',', ':'))
 
+def generate_json(regulation_file, check_terms=False):
+    with open(find_file(regulation_file), 'r') as f:
+        reg_xml = f.read()
+    xml_tree = etree.fromstring(reg_xml)
 
+    # validate relative to schema
+    validator = EregsValidator(settings.XSD_FILE)
+    validator.validate_reg(xml_tree)
+
+    if not validator.is_valid:
+        for event in validator.events:
+            print(str(event))
+        sys.exit(0)
+
+    reg_tree = build_reg_tree(xml_tree)
+    reg_number = reg_tree.label[0]
+
+    paragraph_markers = build_paragraph_marker_layer(xml_tree)
+    internal_citations = build_internal_citations_layer(xml_tree)
+    external_citations = build_external_citations_layer(xml_tree)
+    terms = build_terms_layer(xml_tree)
+    meta = build_meta_layer(xml_tree)
+    toc = build_toc_layer(xml_tree)
+    keyterms = build_keyterm_layer(xml_tree)
+    graphics = build_graphics_layer(xml_tree)
+    formatting = build_formatting_layer(xml_tree)
+    interps = build_interp_layer(xml_tree)
+    analysis = build_analysis(xml_tree)
+    notice_dict = build_notice(xml_tree)
+
+    # if the validator had problems then we should report them and bail out
+
+    validator.validate_terms(xml_tree, terms)
+    validator.validate_internal_cites(xml_tree, internal_citations)
+    if check_terms:
+        validator.validate_term_references(xml_tree, terms, regulation_file)
+    for event in validator.events:
+        print(str(event))
+
+    reg_tree.include_children = True
+    reg_json = reg_tree.to_json()
+
+    notice = xml_tree.find('.//{eregs}documentNumber').text
+    version = os.path.split(regulation_file)[-1].replace('.xml', '')
+    if notice != version:
+        print('Notice ({}) different from version ({}), using version'.format(notice, version))
+        notice = version
+
+    write_layer(reg_json, reg_number, notice, 'regulation')
+    write_layer(meta, reg_number, notice, 'layer/meta')
+    write_layer(paragraph_markers, reg_number, notice,
+                'layer/paragraph-markers')
+    write_layer(internal_citations, reg_number, notice,
+                'layer/internal-citations')
+    write_layer(external_citations, reg_number, notice,
+                'layer/external-citations')
+    write_layer(terms, reg_number, notice, 'layer/terms')
+    write_layer(toc, reg_number, notice, 'layer/toc')
+    write_layer(keyterms, reg_number, notice, 'layer/keyterms')
+    write_layer(graphics, reg_number, notice, 'layer/graphics')
+    write_layer(formatting, reg_number, notice, 'layer/formatting')
+    write_layer(interps, reg_number, notice, 'layer/interpretations')
+    write_layer(analysis, reg_number, notice, 'layer/analyses')
+    write_layer(notice_dict, reg_number, notice, 'notice')
+
+    return reg_number, notice, xml_tree
+
+       
 # Main CLI Commands ####################################################
 
 # Create a general CLI that can take additional comments
@@ -126,7 +189,7 @@ def validate(check_terms, file):
 
     if not validator.is_valid:
         for event in validator.events:
-            logger.error(str(event))
+            print(str(event))
         sys.exit(0)
 
     # Validate regulation-specific documents
@@ -140,7 +203,7 @@ def validate(check_terms, file):
         if check_terms:
             validator.validate_term_references(xml_tree, terms, file)
         for event in validator.events:
-            logger.warning(str(event))
+            print(str(event))
 
     # Validate notice-specific documents
     if xml_tree.tag == '{eregs}notice':
@@ -156,86 +219,25 @@ def validate(check_terms, file):
 # diff JSON will be generated between them.
 @cli.command('json')
 @click.argument('regulation_files', nargs=-1)
-@click.option('--check-terms', default=False)
-def generate_json(regulation_files, check_terms):
+@click.option('--check-terms', is_flag=True)
+def json_command(regulation_files, check_terms=False):
     """ Generate JSON from RegML files """
 
-    # If we've got a list of files, recurse through them one at a time
-    # to build each individual version, then generate diffs for all
-    # versions.
-    if len(regulation_files) > 1:
-        # Generate JSON for each version
-        versions = {}
-        reg_number = None
-        for file in regulation_files:
-            reg_number, notice, reg_xml_tree = generate_json(file, 
-                                                             check_terms)
-            versions[notice] = reg_xml_tree
+    # Generate JSON for each version
+    versions = {}
+    reg_number = None
+    for file in regulation_files:
+        reg_number, notice, reg_xml_tree = generate_json(file,
+            check_terms=check_terms)
+        versions[notice] = reg_xml_tree
 
-        # Generate diff JSON between each version
-        # now build diffs - include "empty" diffs comparing a version to itself
-        for left_version, left_tree in versions.items():
-            for right_version, right_tree in versions.items():
-                diff = generate_diff(left_tree, right_tree)
-                write_layer(diff, reg_number, left_version, 'diff',
-                        diff_notice=right_version)
-
-        return ()
-
-    # Otherwise if we got one file, generate the JSON for it.
-    with open(find_file(regulation_files[0]), 'r') as f:
-        reg_xml = f.read()
-    xml_tree = etree.fromstring(reg_xml)
-
-    # Validate the file relative to schema
-    validator = EregsValidator(settings.XSD_FILE)
-    validator.validate_reg(xml_tree)
-
-    if not validator.is_valid:
-        for event in validator.events:
-            logger.error(str(event))
-        sys.exit(0)
-
-    reg_tree = build_reg_tree(xml_tree)
-    reg_number = reg_tree.label[0]
-
-    paragraph_markers = build_paragraph_marker_layer(xml_tree)
-    internal_citations = build_internal_citations_layer(xml_tree)
-    external_citations = build_external_citations_layer(xml_tree)
-    terms = build_terms_layer(xml_tree)
-    meta = build_meta_layer(xml_tree)
-    toc = build_toc_layer(xml_tree)
-    keyterms = build_keyterm_layer(xml_tree)
-    graphics = build_graphics_layer(xml_tree)
-    formatting = build_formatting_layer(xml_tree)
-    interps = build_interp_layer(xml_tree)
-    analysis = build_analysis(xml_tree)
-    notice_dict = build_notice(xml_tree)
-
-    reg_tree.include_children = True
-    reg_json = reg_tree.to_json()
-
-    notice = xml_tree.find('{eregs}preamble/{eregs}documentNumber').text
-
-    write_layer(reg_json, reg_number, notice, 'regulation')
-    write_layer(meta, reg_number, notice, 'layer/meta')
-    write_layer(paragraph_markers, reg_number, notice,
-                'layer/paragraph-markers')
-    write_layer(internal_citations, reg_number, notice,
-                'layer/internal-citations')
-    write_layer(external_citations, reg_number, notice,
-                'layer/external-citations')
-    write_layer(terms, reg_number, notice, 'layer/terms')
-    write_layer(toc, reg_number, notice, 'layer/toc')
-    write_layer(keyterms, reg_number, notice, 'layer/keyterms')
-    write_layer(graphics, reg_number, notice, 'layer/graphics')
-    write_layer(formatting, reg_number, notice, 'layer/formatting')
-    write_layer(interps, reg_number, notice, 'layer/interpretations')
-    write_layer(analysis, reg_number, notice, 'layer/analyses')
-    write_layer(notice_dict, reg_number, notice, 'notice')
-
-    # Return the XML tree in case we're recursing.
-    return reg_number, notice, xml_tree
+    # Generate diff JSON between each version
+    # now build diffs - include "empty" diffs comparing a version to itself
+    for left_version, left_tree in versions.items():
+        for right_version, right_tree in versions.items():
+            diff = generate_diff(left_tree, right_tree)
+            write_layer(diff, reg_number, left_version, 'diff',
+                    diff_notice=right_version)
 
 
 # Given a notice, apply it to a previous RegML regulation verson to
@@ -269,7 +271,7 @@ def apply(regulation_file, notice_file):
         os.path.dirname(regulation_file),
         os.path.basename(notice_file))
     with open(new_path, 'w') as f:
-        logger.info("Writing regulation to {}".format(new_path))
+        print("Writing regulation to {}".format(new_path))
         f.write(new_xml_string)
 
  
@@ -312,7 +314,7 @@ def ecfr(title, file, act_title, act_section,
     layers = builder.generate_layers(reg_tree, [act_title, act_section], layer_cache)
 
     # Do the first version
-    logger.info("Version %s", builder.doc_number)
+    print("Version %s", builder.doc_number)
     if (only_notice is not None and builder.doc_number == only_notice) or \
             only_notice is None:
         if not without_versions:
@@ -321,7 +323,7 @@ def ecfr(title, file, act_title, act_section,
     for last_notice, old, new_tree, notices in builder.revision_generator(
             reg_tree):
         version = last_notice['document_number']
-        logger.info("Version %s", version)
+        print("Version %s", version)
         builder.doc_number = version
         layers = builder.generate_layers(new_tree, 
                                          [act_title, act_section],
